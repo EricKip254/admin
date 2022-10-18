70097ad28c05d9c44c158f53b0ca5f2e14770162
eric <null>
2022-10-18 12:39:54 +0300

    updated home page header and footer location

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,9 +4,9 @@ class Home extends CI_Controller {
         public function index()
         {
             $data['page_title'] = 'Home';
-            $this->load->view('clientemplates/header');
+            $this->load->view('templates/header');
             $this->load->view('home/home');
-            $this->load->view('clientemplates/footer');
+            $this->load->view('templates/footer');
         }
 }
   
