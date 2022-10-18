c2da529243f14cdb1d290897466a50ffc1cbaed6
eric <null>
2022-10-18 21:23:16 +0300

    Added template

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,9 +4,9 @@ class Dashboard extends CI_Controller {
         public function index()
         {
             //$data['page_title'] = 'Home Page';
-            $this->load->view('templates/lheader');
+            $this->load->view('templates/dheader');
             $this->load->view('dashboard/dashboard');
-            $this->load->view('templates/lfooter');
+            $this->load->view('templates/dfooter');
         }
 }
   
--- application/views/templates/dfooter.php
+++ application/views/templates/dfooter.php
@@ -1,9 +1,6 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
+   
     <!-- Default to the left -->
     <div class="container px-5">
           <div class="row align-items-center justify-content-between flex-column flex-sm-row">
